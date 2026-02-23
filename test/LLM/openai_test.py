dad6cd06fb3aba3691769de7d59a07a4ea447679
정세현 <hyeon@Galaxy-Book.local>
2026-02-23 22:53:09 +0900

    26.02.23 KIS dataloader를 프로젝트 목적에 맞게 수정중...

---
+++ test/LLM/openai_test.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.responses.create(
+    model='gpt-5-nano',                     # 모델 선택
+    reasoning={'effort': 'medium'},         # 추론 정도
+    input="사용가능한 openai의 모델 종류"        
+)
+
+print(response.output_text)
